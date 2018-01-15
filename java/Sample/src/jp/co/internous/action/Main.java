cf08e98817698a08b3dd96dd50da6d0f105bddab
termite021 <hgsk6gn@gmail.com>
2018-01-15 21:38:53 +0900

    sended 1/15

---
+++ java/Sample/src/jp/co/internous/action/Main.java
@@ -0,0 +1,23 @@
+package jp.co.internous.action;
+
+public class Main {
+
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println(warizan(10,10));
+		System.out.println(kakezan(10,10));
+		System.out.println(hikizan(10,10));
+	}
+
+	public static double warizan(int a,int b){
+		return a/b;
+	}
+	public static double kakezan(int a,int b){
+		return a*b;
+	}
+	public static double hikizan(int a,int b){
+		return a-b;
+	}
+
+}
---
+++ java/human/src/jp/co/internous/action/HumanAddres.java
@@ -0,0 +1,11 @@
+package jp.co.internous.action;
+
+public class HumanAddres {
+
+	public String addres;
+	public String getAddres(){
+		addres = "Tokyo";
+		return addres;
+	}
+
+}
